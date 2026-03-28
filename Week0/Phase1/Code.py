d746ef5ddc00e61d64a05c7820d5811593ccdd99
Manasa0109 <155514045+Manasa0109@users.noreply.github.com>
2026-03-28 16:12:41 +0530

    Update Code.py

--- Week0/Phase1/Code.py
+++ Week0/Phase1/Code.py
@@ -7,7 +7,12 @@ customers_data = [
     (4, "Meena", "Bengaluru", 35),
     (5, "Kiran", "Chennai", 22)
 ]
-customers = spark.createDataFrame(
-    customers_data,
-    ["customer_id", "customer_name", "city", "age"]
-)
+orders_data = [
+    (101, 1, 2500, "2026-03-01"),
+    (102, 2, 1800, "2026-03-02"),
+    (103, 1, 3200, "2026-03-03"),
+    (104, 3, 1500, "2026-03-04"),
+    (105, 5, 2800, "2026-03-05")
+]
+customers = spark.createDataFrame(customers_data, ["customer_id","customer_name","city","age"])
+orders = spark.createDataFrame(orders_data, ["order_id","customer_id","amount","order_date"])
